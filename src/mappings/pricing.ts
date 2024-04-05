e1b0703b8c95c03d9a4597bb3681dc419936dde1
RitzyDev <RitzyDevBox@proton.me>
2024-04-05 13:13:10 -0400

    adding support for bttc graph

--- src/mappings/pricing.ts
+++ src/mappings/pricing.ts
@@ -3,49 +3,44 @@ import { Pair, Token, Bundle } from '../types/schema'
 import { BigDecimal, Address, BigInt, ethereum } from '@graphprotocol/graph-ts/index'
 import { ZERO_BD, factoryContract, ADDRESS_ZERO, ONE_BD, UNTRACKED_PAIRS } from './helpers'
 
-const WETH_ADDRESS = '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83'
-const USDC_MULTI_WETH_PAIR = '0x2b4c76d0dc16be1c31d4c1dc53bf9b45987fc75c' // created at block 3802001 usdc is token 0
-const USDC_LZ_WETH_PAIR = '0x90469ACbC4b6d877873CD4f1CCA54fDE8075A998'  // created at block 65245749 usdc is token 1
-const USDC_AXL_WETH_PAIR = '0xa196c7754f4ec79de55bb5db82187bbe82275f7f' // created at block 65318511 usdc is token 0
+const WNATIVE_ADDRESS = '0x23181F21DEa5936e24163FFABa4Ea3B316B57f3C'
+const USDT_T_WBTT = '0x418AFCD6D15fA51d59d6C7F7A5943eB04D5F21f0' // created block 26857087
+const USDC_WBTT = '0x98A647f4A1Fe1dc58Fc1A107468151bBe2b5A627' // created 27823408 
 
 export function getEthPriceInUSD(event: ethereum.Event): BigDecimal {
-  
-  // We will use the Multi_USDC as the graph pair until we have a new pair to use as reference
-  if(event.block.number < BigInt.fromI32(65245749)) {
-    let usdcMultiPair = Pair.load(USDC_MULTI_WETH_PAIR); // Usdc is token0
-    if (usdcMultiPair !== null) {
-      return usdcMultiPair.token0Price
-    }
-    else {
-      return ZERO_BD
-    }
-  }
-  
   // fetch eth prices for each stablecoin
-  let usdcAxlPair = Pair.load(USDC_AXL_WETH_PAIR) // usdc_axl is token0
-  let usdcLzPair = Pair.load(USDC_LZ_WETH_PAIR) // usdc_lz is token1
+  let usdtNativePair = Pair.load(USDT_T_WBTT) // usdc is token1
+  let usdcNativePair = Pair.load(USDC_WBTT) // usdc is token1
 
-  if (usdcAxlPair !== null && usdcLzPair !== null) {
-    let totalLiquidityETH = usdcAxlPair.reserve1.plus(usdcLzPair.reserve0)
-    let axlUSdcWeight = usdcAxlPair.reserve1.div(totalLiquidityETH)
-    let lzUsdcWeight = usdcLzPair.reserve0.div(totalLiquidityETH)
-    return usdcAxlPair.token0Price.times(axlUSdcWeight).plus(usdcLzPair.token1Price.times(lzUsdcWeight))
+if (usdtNativePair !== null && usdcNativePair !== null) {
+    let totalLiquidityETH = usdtNativePair.reserve0.plus(usdcNativePair.reserve0)
+    let stableWight = usdtNativePair.reserve0.div(totalLiquidityETH)
+    let otherStableWeight = usdcNativePair.reserve0.div(totalLiquidityETH)
+    return usdtNativePair.token1Price.times(stableWight).plus(usdcNativePair.token1Price.times(otherStableWeight))
     // USDC is the only pair so far
-  } else if (usdcLzPair !== null) {
-    return usdcLzPair.token1Price
+  } else if (usdtNativePair !== null) {
+    return usdtNativePair.token1Price
   } else {
     return ZERO_BD
   }
 }
 
+
 // token where amounts should contribute to tracked volume and liquidity
 let WHITELIST: string[] = [
-  '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83', // wftm
-  '0x04068da6c83afcfa0e13ba15a6696662335d5b75', // USDC
-  '0x1b6382dbdea11d97f24495c9a90b7c88469134a4', // axlUSDC
-  '0xfe7eda5f2c56160d406869a8aa4b2f365d544c7b', // axlETH
-  '0x28a92dde19d9989f39a49905d7c9c2fac7799bdf', // lzUSDC
-  '0x695921034f0387eac4e11620ee91b1b15a6a09fe', // lzETH
+  WNATIVE_ADDRESS,
+  '0xAD9A21FF0c9d854cA8C1360AF28D4fcbDaC53B4F', // FTM_e
+  '0x43559B1786C06d6B826e3cf9AA667eD8840f9106', // ADA_b
+  '0x1249C65AfB11D179FFB3CE7D4eEDd1D9b98AD006', // ETH
+  '0x17F235FD5974318E4E2a5e37919a209f7c37A6d1', // USDD_t
+  '0xdB28719F7f938507dBfe4f0eAe55668903D34a15', // USDT_t
+  '0xE887512ab8BC60BcC9224e1c3b5Be68E26048B8B', // USDT_e
+  '0xAE17940943BA9440540940DB0F1877f101D39e8b', // USDC_e
+  '0xCa424b845497f7204D9301bd13Ff87C0E2e86FCF', // USDC_b
+  '0x9888221fE6B5A2ad4cE7266c7826D2AD74D40CcF', // WBTC__e
+  '0xfd3b093aB6bD4F40810f19e5fF822ac8Cc7e3184', // LINK_e
+  '0xEdf53026aeA60f8F75FcA25f8830b7e2d6200662', // TRX
+  '0x185a4091027E2dB459a2433F85f894dC3013aeB5', // BNB
 ]
 
 // minimum liquidity required to count towards tracked volume for pairs with small # of Lps
@@ -59,7 +54,7 @@ let MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('200')
  * @todo update to be derived ETH (add stablecoin estimates)
  **/
 export function findEthPerToken(token: Token): BigDecimal {
-  if (token.id == WETH_ADDRESS) {
+  if (token.id == WNATIVE_ADDRESS) {
     return ONE_BD
   }
   // loop through whitelist and check if paired with any
