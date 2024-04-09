60cf82413f35dc40a3279d56b6ea2833059836bb
RitzyDev <RitzyDevBox@proton.me>
2024-04-09 18:17:32 -0400

    fixing address to be in lowercase form

--- src/mappings/pricing.ts
+++ src/mappings/pricing.ts
@@ -3,8 +3,8 @@ import { Pair, Token, Bundle } from '../types/schema'
 import { BigDecimal, Address, BigInt, ethereum } from '@graphprotocol/graph-ts/index'
 import { ZERO_BD, factoryContract, ADDRESS_ZERO, ONE_BD, UNTRACKED_PAIRS } from './helpers'
 
-const WNATIVE_ADDRESS = '0x23181F21DEa5936e24163FFABa4Ea3B316B57f3C'
-const USDT_T_WBTT = '0x418AFCD6D15fA51d59d6C7F7A5943eB04D5F21f0' // created block 26857087
+const WNATIVE_ADDRESS = '0x23181f21dea5936e24163ffaba4ea3b316b57f3c'
+const USDT_T_WBTT = '0x418afcd6d15fa51d59d6c7f7a5943eb04d5f21f0' // created block 26857087
 
 export function getEthPriceInUSD(event: ethereum.Event): BigDecimal {
   // fetch eth prices for each stablecoin
@@ -19,19 +19,19 @@ export function getEthPriceInUSD(event: ethereum.Event): BigDecimal {
 
 // token where amounts should contribute to tracked volume and liquidity
 let WHITELIST: string[] = [
-  '0x23181F21DEa5936e24163FFABa4Ea3B316B57f3C', // WBTTC
-  '0xAD9A21FF0c9d854cA8C1360AF28D4fcbDaC53B4F', // FTM_e
-  '0x43559B1786C06d6B826e3cf9AA667eD8840f9106', // ADA_b
-  '0x1249C65AfB11D179FFB3CE7D4eEDd1D9b98AD006', // ETH
-  '0x17F235FD5974318E4E2a5e37919a209f7c37A6d1', // USDD_t
-  '0xdB28719F7f938507dBfe4f0eAe55668903D34a15', // USDT_t
-  '0xE887512ab8BC60BcC9224e1c3b5Be68E26048B8B', // USDT_e
-  '0xAE17940943BA9440540940DB0F1877f101D39e8b', // USDC_e
-  '0xCa424b845497f7204D9301bd13Ff87C0E2e86FCF', // USDC_b
-  '0x9888221fE6B5A2ad4cE7266c7826D2AD74D40CcF', // WBTC__e
-  '0xfd3b093aB6bD4F40810f19e5fF822ac8Cc7e3184', // LINK_e
-  '0xEdf53026aeA60f8F75FcA25f8830b7e2d6200662', // TRX
-  '0x185a4091027E2dB459a2433F85f894dC3013aeB5', // BNB
+  '0x23181f21dea5936e24163ffaba4ea3b316b57f3c', // WBTTC
+  '0xad9a21ff0c9d854ca8c1360af28d4fcbdac53b4f', // FTM_e
+  '0x43559b1786c06d6b826e3cf9aa667ed8840f9106', // ADA_b
+  '0x1249c65afb11d179ffb3ce7d4eedd1d9b98ad006', // ETH
+  '0x17f235fd5974318e4e2a5e37919a209f7c37a6d1', // USDD_t
+  '0xdb28719f7f938507dbfe4f0eae55668903d34a15', // USDT_t
+  '0xe887512ab8bc60bcc9224e1c3b5be68e26048b8b', // USDT_e
+  '0xae17940943ba9440540940db0f1877f101d39e8b', // USDC_e
+  '0xca424b845497f7204d9301bd13ff87c0e2e86fcf', // USDC_b
+  '0x9888221fe6b5a2ad4ce7266c7826d2ad74d40ccf', // WBTC__e
+  '0xfd3b093ab6bd4f40810f19e5ff822ac8cc7e3184', // LINK_e
+  '0xedf53026aea60f8f75fca25f8830b7e2d6200662', // TRX
+  '0x185a4091027e2db459a2433f85f894dc3013aeb5', // BNB
 ]
 
 // minimum liquidity required to count towards tracked volume for pairs with small # of Lps
